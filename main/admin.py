458f4e34de91da9bd1224fceaa30f2ffc6041ac8
Yu-fei Li <sanguo0023@gmail.com>
2014-03-02 16:25:51 +0800

    Update index

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from main.models import *
+admin.site.register(Goods)
+admin.site.register(Stock)
+admin.site.register(Outer)
+admin.site.register(Inner)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Operator)
